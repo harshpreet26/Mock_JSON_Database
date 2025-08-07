567bfcf1ace9e086726085925d42152256e665be
harshpreet26 <harshmaur2004@gmail.com>
2025-08-07 11:05:32 -0600

    commit

--- apps.js
+++ apps.js
@@ -2,8 +2,15 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI;
 
 
+
+mongoose.connect(uri).then(
+async () => {
 app.listener(PORT, () => {
-  console.log(`connected to port ${port}`);
+  console.log(`connected to port ${port}`)
 });
+
+}).catch(err => { console.log(`error: ${err}`) });
